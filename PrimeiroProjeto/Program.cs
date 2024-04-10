2eebb50cc09dce16260495405fa4a8ab53644c0c
Ábner Santin <93490804+http-AbnerSantin@users.noreply.github.com>
2024-04-10 14:45:24 -0300

    Update Program.cs

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
@@ -22,25 +22,18 @@ namespace PrimeiroProjeto // Note: actual namespace depends on the project name.
             Console.WriteLine("Informe o valor de C");
             C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if(A > 0)
-            {
-                delta = Math.Pow(B, 2) - 4 * A * C;
+            delta = Math.Pow(B, 2) - 4 * A * C;
 
-                if(delta > 0)
-                {
-
-                    X1 = (double)(-B - Math.Sqrt(delta)) / (2 * A);
-                    X2 = (double)(-B + Math.Sqrt(delta)) / (2 * A);
-
-                    Console.WriteLine("X1 = " + X1);
-                    Console.WriteLine("X2 = " + X2);
-                } else
-                {
-                    Console.WriteLine("Delta negativo");
-                }
-            } else
+            if(A == 0 || delta < 0.0)
             {
                 Console.WriteLine("Impossivel calcular");
+            } else
+            {
+                X1 = (double)(-B - Math.Sqrt(delta)) / (2 * A);
+                X2 = (double)(-B + Math.Sqrt(delta)) / (2 * A);
+
+                Console.WriteLine("R1 = " + X1.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + X2.ToString("F2", CultureInfo.InvariantCulture));
             }
         }
     }
